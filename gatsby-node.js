66d7b2217166998406849927231f2d2ae56f062e
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-05-06 02:46:45 -0300

    removind date

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,9 +5,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     const result = await graphql(`
     {
         allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
-                limit: 1000
-            ) {
+            sort: {
                 edges {
                     node {
                         frontmatter {
